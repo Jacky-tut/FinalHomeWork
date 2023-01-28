e1c053e6285eae5691354f745740981d370d5269
Jacky_tut <netfinnov@mail.ru>
2023-01-28 16:20:51 +0300

    create homework

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿string[] GetNewArray(string[] newArray)
+{
+    int lengthNewArray = 0;
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length <= 3) 
+        {
+            lengthNewArray++;
+        }
+    }
+
+    string[] resultArray = new string[lengthNewArray];
+    int j = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length <= 3) 
+        {
+            resultArray[j] = newArray[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]} ]");
+}
+
+string[] newArray = {"Hello", "2", "World", ":-)"};
+string[] resultArray = GetNewArray(newArray);
+PrintArray(newArray);
+Console.Write(" -> ");
+PrintArray(resultArray);
+Console.WriteLine();
